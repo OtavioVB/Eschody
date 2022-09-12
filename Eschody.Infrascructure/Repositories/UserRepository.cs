ee90b51da81349b1a168f913c31763aa6cd760ce
OtavioVB <otaviovb.developer@gmail.com>
2022-09-12 16:25:24 -0300

    refactor(repository): `user identity`

--- Eschody.Infrascructure/Repositories/UserRepository.cs
+++ Eschody.Infrascructure/Repositories/UserRepository.cs
@@ -1,5 +1,27 @@
-﻿namespace Eschody.Infrascructure.Repositories;
+﻿using Eschody.Domain.Contracts.Infrascructure.Repositories;
+using Microsoft.AspNetCore.Identity;
 
-public class UserRepository
+namespace Eschody.Infrascructure.Repositories;
+
+public class UserRepository : IBaseRepository<IdentityUser>
 {
+    public Task DeleteAsync(IdentityUser entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityUser?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertAsync(IdentityUser entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(IdentityUser entity)
+    {
+        throw new NotImplementedException();
+    }
 }
