c6d7f7bc6c270cfe0df415e26de8446db2c1efbf
OtavioVB <otaviovb.developer@gmail.com>
2022-09-13 14:29:09 -0300

    fix(header): `header`

---
+++ Eschody.Application/wwwroot/content_app/javascript/header/function.js
@@ -0,0 +1,11 @@
+﻿document.querySelector('.icon-menu').addEventListener("click", function openMenu() {
+    let MenuMobile = document.querySelector('.mobile-menu');
+    if (MenuMobile.classList.contains('open')) {
+        MenuMobile.classList.remove('open');
+        document.querySelector('.mobile-menu-icon-icon').src = "Assets/content_app/menu_white_36dp.svg";
+    }
+    else {
+        MenuMobile.classList.add('open');
+        document.querySelector('.mobile-menu-icon-icon').src = "Assets/content_app/close_white_36dp.svg";
+    }
+});
