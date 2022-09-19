206ca218b707e298e8e241dda5069845456ba211
OtavioVB <otaviovb.developer@gmail.com>
2022-09-19 20:40:34 -0300

    feat(handlers): `UserLoginHandler`

---
+++ Eschody.Domain/Contracts/Services/IHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Eschody.Domain.Contracts.Services;
+
+public interface IHandler<Request, Response> where Request : IRequest where Response : IResponse
+{
+    public Response Handle(Request request);
+}
