c9b2ad196bb9d6d124a50daf4c797f03bf833eda
OtavioVB <otaviovb.developer@gmail.com>
2022-09-19 22:13:33 -0300

    fix(workflows): `implement steps`

--- Eschody.Services/Handlers/Authentication/Register/UserHandlerRegister.cs
+++ Eschody.Services/Handlers/Authentication/Register/UserHandlerRegister.cs
@@ -50,6 +50,8 @@ public class UserHandlerRegister : Notifiable, IHandler<UserRequest, UserRespons
             return response;
         }
 
+
+
         var user = new User()
         {
             Created = DateTime.Now,
