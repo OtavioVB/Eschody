2991db844780df474b4bc3a915c995325ffa63b0
OtavioVB <otaviovb.developer@gmail.com>
2022-10-12 19:53:05 -0300

    feat(assignment_repository): `Database requests`

---
+++ Eschody.Infrascructure/Repositories/Business/AssignmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Eschody.Domain.Models.DTOs;
+using Eschody.Infrascructure.Data;
+
+namespace Eschody.Infrascructure.Repositories.Business;
+
+public class AssignmentRepository
+{
+    private readonly DataContext _dataContext;
+
+    public AssignmentRepository(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+    
+    /// <summary>
+    /// Inserir uma nova tarefa no banco de dados
+    /// </summary>
+    /// <param name="assignment">Assignment Entity</param>
+    public async Task InsertNewAssignment(Assignment assignment)
+    {
+        await _dataContext.Assignments.AddAsync(assignment);
+        await _dataContext.SaveChangesAsync();
+    }
+}
